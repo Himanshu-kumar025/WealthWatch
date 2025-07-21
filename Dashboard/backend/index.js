052c046d07a0de41bf4f78f49cd7036b4eb3e9d5
Himanshu-kumar025 <hk1832141@gmail.com>
2025-07-21 17:56:39 +0530

    ready for deploy4

--- Dashboard/backend/index.js
+++ Dashboard/backend/index.js
@@ -32,7 +32,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Enable CORS for frontend apps
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://wealth-watch-dashboard-one.vercel.app/'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
