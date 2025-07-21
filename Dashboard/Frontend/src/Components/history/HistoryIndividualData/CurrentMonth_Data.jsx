ea226ab9082fbf51b4ebf1c82b9056d49f245947
Himanshu-kumar025 <hk1832141@gmail.com>
2025-07-21 17:50:12 +0530

    ready for deploy3

--- Dashboard/Frontend/src/Components/history/HistoryIndividualData/CurrentMonth_Data.jsx
+++ Dashboard/Frontend/src/Components/history/HistoryIndividualData/CurrentMonth_Data.jsx
@@ -4,7 +4,7 @@ import {
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import { BASE_URL } from "../../../../../backend/axiosConfig.js";
+import { BASE_URL } from "../../axiosConfig.js";
 import { fetchMonthlyData } from "../../data/InputData.js";
 
 export default function CurrentHistoryIndividual() {
